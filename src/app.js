739065444a09b621ab35cd5639b0b96e6a3d8df8
jockdeveloper <jockdeveloper@gmail.com>
2022-09-18 21:36:39 +0200

    Añadiendo texturas

--- src/app.js
+++ src/app.js
@@ -13,19 +13,28 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(0, 0, 15);
 
+// Texturas
+const textureLoader = new THREE.TextureLoader();
+textureLoader.setPath('./src/assets/textures/');
+const baseColor = textureLoader.load('base_color.jpg');
+const roughness = textureLoader.load('metallic_roughness.png');
+const normalMap = textureLoader.load('normal_map.png');
+
 // Material
 const material = new THREE.MeshBasicMaterial({
-    color: 'teal',
+    map: baseColor
 });
 
 const material_standar = new THREE.MeshStandardMaterial({
-    color: 'coral',
-    flatShading: true,
-    roughness: 0.1
+    map: baseColor,
+    roughnessMap: roughness,
+    normalMap: normalMap
 });
 
+// Geometrias
+const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+
 // Mesh
-const geometry = new THREE.CylinderBufferGeometry(1, 1, 3, 8);
 const mesh = new THREE.Mesh(geometry, material);
 mesh.rotateZ(THREE.MathUtils.degToRad(90));
 mesh.position.set(-3, 0, 0);
@@ -40,22 +49,12 @@ mesh2.position.set(3, 0, 0);
 scene.add(mesh2);
 
 // Luces
-const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xff0000, 1);
+const hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.5);
 scene.add(hemisphereLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0.8, 2, 4);
 scene.add(directionalLight);
-
-// Helpers
-const helperAxes = new THREE.AxesHelper(10);
-scene.add(helperAxes);
-
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
-scene.add(directionalLightHelper);
-
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight);
-scene.add(hemisphereLightHelper);
 
 // Render
 const renderer = new THREE.WebGLRenderer({
