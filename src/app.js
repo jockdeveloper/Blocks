18f228d6937cb83c610c1cec6d3ab9ba63eee457
jockdeveloper <jockdeveloper@gmail.com>
2022-09-18 20:57:47 +0200

    Creando cilindros y añdiendo luces y helpers

--- src/app.js
+++ src/app.js
@@ -13,24 +13,63 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(0, 0, 15);
 
-// Mesh
-const geometry = new THREE.BoxGeometry(2, 2, 2);
+// Material
 const material = new THREE.MeshBasicMaterial({
     color: 'teal',
 });
-const box_mesh = new THREE.Mesh(geometry, material);
-scene.add(box_mesh);
+
+const material_standar = new THREE.MeshStandardMaterial({
+    color: 'coral',
+    flatShading: true,
+    roughness: 0.1
+});
+
+// Mesh
+const geometry = new THREE.CylinderBufferGeometry(1, 1, 3, 8);
+const mesh = new THREE.Mesh(geometry, material);
+mesh.rotateZ(THREE.MathUtils.degToRad(90));
+mesh.position.set(-3, 0, 0);
+
+scene.add(mesh);
+
+// Mesh 2
+const mesh2 = new THREE.Mesh(geometry, material_standar);
+mesh2.rotateZ(THREE.MathUtils.degToRad(90));
+mesh2.position.set(3, 0, 0);
+
+scene.add(mesh2);
+
+// Luces
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xff0000, 1);
+scene.add(hemisphereLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight.position.set(0.8, 2, 4);
+scene.add(directionalLight);
+
+// Helpers
+const helperAxes = new THREE.AxesHelper(10);
+scene.add(helperAxes);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+scene.add(directionalLightHelper);
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight);
+scene.add(hemisphereLightHelper);
 
 // Render
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setSize(container.clientWidth, container.clientHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.physicallyCorrectLights = true;
 
 container.appendChild(renderer.domElement);
 
 const update = () => {
-    box_mesh.rotateX(0.01);
-    box_mesh.rotateY(0.01);
+    mesh.rotateY(0.01);
+    mesh2.rotateY(0.01);
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(() => update());
